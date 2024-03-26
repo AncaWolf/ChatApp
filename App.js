236ede8c56e4294b573e35f68eedb9062127a544
Anca <anca.m.gozman@gmail.com>
2024-03-26 17:49:29 +0100

    App created
    Task 5-1
    - created app with create-expo-app --template
    - created README
    - added BackgroundImage.png to assets
    - created components folder, and Start.js and Chat.js

--- App.js
+++ App.js
@@ -1,12 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+// import screens
+import StartScreen from './components/Start';
+import ChatScreen from './components/Chat';
+// import React navigation
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-export default function App() {
+// Create the navigator
+const Stack = createNativeStackNavigator();
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Start"
+      >
+        <Stack.Screen
+          name="Start"
+          component={StartScreen}
+        />
+        <Stack.Screen
+          name="Chat"
+          component={ChatScreen}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
@@ -18,3 +37,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default App;
